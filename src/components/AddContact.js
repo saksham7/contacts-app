8eb32171bf8b8c5e6189f190a9facc8fcab10fdf
saksham7 <sakshamsrivastava74@gmail.com>
2021-07-19 17:21:36 +0530

    minor fixes

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -7,7 +7,7 @@ class AddContact extends React.Component {
   };
   add = (e) => {
     e.preventDefault();
-    if (this.state.name == "" || this.state.mobile == "") {
+    if (this.state.name === "" || this.state.mobile === "") {
       alert("Please fill all fields!");
       return;
     }
--- src/components/ContactDetail.js
+++ src/components/ContactDetail.js
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
 import user from "../images/user-avatar.png";
 const ContactDetail = (props) => {
-  const { id, name, mobile } = props.location.state.contact;
+  const { name, mobile } = props.location.state.contact;
   return (
     <div className="ui main">
       <div className=" ui card centered">
-        <img src={user} className="image"></img>
+        <img src={user} alt="User Image" className="image"></img>
         <div className="content">
           <div className="header">{name}</div>
           <p className="description">{mobile}</p>
